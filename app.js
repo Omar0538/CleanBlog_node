4c844c3ae1ff15d7a9832490f3d2bf75a85b7cb0
Omar38 <omar.sultanov.98@mail.ru>
2021-11-18 15:13:46 +0300

    Add static files and ejs engine

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express=require("express");
+const ejs=require("ejs")
 const app=express();
 const port=3000;
+
+app.use(express.static('public'));
+app.set("view engine","ejs");
 app.get("/",(req,res)=>{
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-   res.send(blog)
+    res.render("index")
+})
+app.get("/add_post",(req,res)=>{
+    res.render("add_post")
+})
+app.get("/post",(req,res)=>{
+    res.render("post")
+})
+app.get("/about",(req,res)=>{
+    res.render("about")
 })
 app.listen(port,()=>{
     console.log(`Our server is getting start with this port:${port}`);
