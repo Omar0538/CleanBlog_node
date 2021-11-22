973250c036345c5a6c60fd5fefe4d4d7161f7204
Omar38 <omar.sultanov.98@mail.ru>
2021-11-22 16:22:07 +0300

    Add mongoose and req.body

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 const express=require("express");
-const ejs=require("ejs")
 const app=express();
+const mongoose = require('mongoose');
+const { urlencoded, json } = require('express');
+const ejs=require("ejs");
+
+const Post=require('./models/Post')
+
 const port=3000;
 
-app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+//template engine
 app.set("view engine","ejs");
-app.get("/",(req,res)=>{
-    res.render("index")
+
+//middlewares
+app.use(express.static('public'));
+app.use(urlencoded({ extended: false }))
+app.use(json())
+
+
+app.get('/',async (req,res)=>{
+    const posts= await Post.find({});
+    res.render("index",{
+        posts
+    })
 })
-app.get("/add_post",(req,res)=>{
+app.get('/add_post',(req,res)=>{
     res.render("add_post")
 })
-app.get("/post",(req,res)=>{
+app.get('/post',(req,res)=>{
     res.render("post")
 })
-app.get("/about",(req,res)=>{
+app.get('/about',(req,res)=>{
     res.render("about")
 })
+
+app.post('/posts',(req, res) => {
+ Post.create(req.body)
+    res.redirect("/")
+  });
 app.listen(port,()=>{
     console.log(`Our server is getting start with this port:${port}`);
 })
